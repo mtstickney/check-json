be21a3ff435741ddcea4e9aa91f18a57e2339f14
Matthew Stickney <fiendishlinux@gmail.com>
2013-05-23 23:15:08 -0400

    Export useful symbols

--- package.lisp
+++ package.lisp
@@ -1,7 +1,14 @@
 ;;;; package.lisp
 
 (defpackage #:check-json
-  (:use #:cl))
+  (:use #:cl)
+  (:export :obj-type-errors
+           :error-template
+           :render-error-template
+           :invalid-template-key
+           :print-json-path
+           :error-report
+           :validate-json))
 
 (defpackage #:json-bool
   (:export :true
